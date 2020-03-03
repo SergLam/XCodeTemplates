0b0712ead5a92489d66adf82c8043b45f58f4afe
Sergey Lamcev <sergey.lamcev@anoda.mobi>
2020-03-03 22:33:01 +0200

    Coordinator + VIPER - Templete Info plist clean up

--- File Templates/Protocols/Viper+Coordinator_SWIFT.xctemplate/Navigation/Coordinator.swift
+++ File Templates/Protocols/Viper+Coordinator_SWIFT.xctemplate/Navigation/Coordinator.swift
@@ -1,9 +1,13 @@
 //___FILEHEADER___
 
 import UIKit
+
 // https://www.hackingwithswift.com/articles/71/how-to-use-the-coordinator-pattern-in-ios-apps
 // https://www.hackingwithswift.com/articles/175/advanced-coordinator-pattern-tutorial-ios
+
 // https://medium.com/@pavlepesic/flow-coordination-pattern-5eb60cd220d5
+
+// https://academy.realm.io/posts/mobilization-lukasz-mroz-mvvm-coordinators-rxswift/
 
 protocol Coordinator: class {
     
