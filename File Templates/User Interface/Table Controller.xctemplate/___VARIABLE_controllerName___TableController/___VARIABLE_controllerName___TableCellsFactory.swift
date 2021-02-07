0d6cdb8f6f3466e751852f5a679b20f36c8c9c60
SergLam <s.lamcev@newlinetechnologies.net>
2021-02-07 02:43:13 +0200

    classic table controller template

---
+++ File Templates/User Interface/Table Controller.xctemplate/___VARIABLE_controllerName___TableController/___VARIABLE_controllerName___TableCellsFactory.swift
@@ -0,0 +1,82 @@
+//___FILEHEADER___
+
+import UIKit
+
+protocol ___VARIABLE_controllerName___TableCellsFactoryDelegate: class {
+    
+}
+
+final class ___VARIABLE_controllerName___TableCellsFactory: TableCellsFactory {
+    
+    weak var delegate: ___VARIABLE_controllerName___TableCellsFactoryDelegate?
+    
+    var cellTypes: [TableViewCell.Type] {
+        [
+            ___VARIABLE_cellType___.self
+        ]
+    }
+    
+    var headerFooterTypes: [TableViewHeaderFooterView.Type] {
+        [
+            ___VARIABLE_headerFooterType___.self
+        ]
+    }
+    
+    func registerAllCells(for tableView: UITableView) {
+        
+        cellTypes.forEach{
+            let bundle = Bundle(for: $0)
+            let nib = UINib(nibName: $0.reuseIdentifier, bundle: bundle)
+            tableView.register(nib, forCellReuseIdentifier: $0.reuseIdentifier)
+        }
+        headerFooterTypes.forEach{
+            let bundle = Bundle(for: $0)
+            let nib = UINib(nibName: $0.reuseIdentifier, bundle: bundle)
+            tableView.register(nib, forHeaderFooterViewReuseIdentifier: $0.reuseIdentifier)
+        }
+    }
+    
+    func generateCell(for viewModel: TableCellModel, tableView: UITableView, at indexPath: IndexPath) -> UITableViewCell {
+        
+        switch viewModel.type {
+        default:
+            
+            // NOTE: - dequeueReusableCell example = cell update
+//            guard let model = viewModel as? MyProfileHeaderTableViewCellVM else {
+//                assertionFailure("Invalid model type")
+//                return UITableViewCell()
+//            }
+//            let cell = view.dequeueReusableCell(MyProfileHeaderTableViewCell.self, for: indexPath)
+//            cell.configureWithModel(model)
+//            return cell
+            
+            assertionFailure("Invalid cell type")
+            return UITableViewCell()
+        }
+    }
+    
+    func generateHeader(for viewModel: TableHeaderFooterModel, tableView: UITableView, at section: Int) -> UIView? {
+        
+        switch section {
+        case 0:
+            
+//            let header = tableView.dequeueReusable(headerFooterView: MyPublicationsListHeaderView.self)
+//            guard let model = viewModel as? MyPublicationsListHeaderViewVM else {
+//                return nil
+//            }
+//            header.update(with: model)
+//            return header
+            
+            return nil
+            
+        default:
+            return nil
+        }
+    }
+    
+    func generateFooter(for viewModel: TableHeaderFooterModel, tableView: UITableView, at section: Int) -> UIView? {
+        
+        return nil
+    }
+    
+}
